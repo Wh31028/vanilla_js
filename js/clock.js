6e99f853a19cbd69dc0e5a6901571ee0debbb41b
wh31028 <wh31028@naver.com>
2023-12-21 15:55:49 +0900

    5.2 padstart
    number를 string으로 바꾸는 방법은 String() 안에 감싸는 방법이 있다.
    
    0> 00 로 바꾸고 싶을 때는 padStart(x, 'y') 를 사용하면 된다.
    padStart는 내가 갖고 있는 string 을 길게 만들어 주고 싶을 때 사용.
    뒤에서 추가하고 싶을 때는 padEnd를 사용하면 된다.
    x는 원하는 length를, y는 앞에 채워줄 내용을 입력하면 된다.

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("hello");
+function getCLock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(sayHello, 5000);
+getCLock(); //website가 실행되자마자 실행하기 위해서
+setInterval(getCLock, 1000);
